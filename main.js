60235bba8fb9d874a7cd33499a22a3495ebeba51
marzyahh <marzyahhooseini74@gmail.com>
2021-01-02 13:12:36 +0330

    a litte study

---
+++ main.js
@@ -0,0 +1,6 @@
+// it's a little difference about other topics, so i create new file 
+// JS Callbacks:::::
+// sometimes we want call a function in an an other function, just like an argument(run first fun throw second ones )
+// and even we can run the second fun after running first fun has done(finished)
+// so I think I don't learn very well, I think I need to learn them  in project more
+// I study about callback, asynchronous , Promise and wait but I don't understood very well
